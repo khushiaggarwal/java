912053d2ee52419a1bdd472406ab9e5204980207
khushiaggarwal <77402725+khushiaggarwal@users.noreply.github.com>
2021-08-27 18:02:36 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+  public static void main(String args[]) {
+    String A = "hello ";
+    String B = "world";
+
+    System.out.println(A + B);
+  }
+}
---
+++ sakshi.java
@@ -0,0 +1,5 @@
+public class sakshi{
+ public static void main(String args[]){
+System.out.println("sakshi is a traitor");
+}
+}
